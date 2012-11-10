a552379298ea39930f13f1477e6e483e5699fb82
Sam Saccone <samccone@gmail.com>
2012-11-09 19:59:54 -0500

    wrap server setup in config block

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ var webServer     = http.createServer(app);
 var colors        = require("colors");
 var io            = require("socket.io");
 
-app.use(express.static(__dirname+"/public"));
-app.use(express.bodyParser());
-app.use(express.cookieParser(process.env.COOKIE_SECRET || "zip zap foo"));
-app.use(express.session({'cookie':{'maxAge':604800000}, 'secret': process.env.COOKIE_SECRET || "zip zap foo"}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.set("view engine", "jade");
+app.configure(function() {
+  app.use(express.static(__dirname+"/public"));
+  app.use(express.bodyParser());
+  app.use(express.cookieParser(process.env.COOKIE_SECRET || "zip zap foo"));
+  app.use(express.session({'cookie':{'maxAge':604800000}, 'secret': process.env.COOKIE_SECRET || "zip zap foo"}));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.set("view engine", "jade");
+});
 
 /*
  * Passport session setup

3085ddd622ce7c2322b8d2310843b277637095a6
Will Farrell <will@mojotech.com>
2012-11-10 11:44:07 -0500

    [wip]

--- public/js/client.js
+++ public/js/client.js
@@ -17,35 +17,40 @@ window.onload = function(){
 
 	Crafty.scene("main", function () {
 
-		Crafty.c("LeftControls", {
-		    init: function() {
-		        this.requires('Multiway');
-		    },
+		// player
+		// bullet
+		// enemy
+		// sun
 
-		    leftControls: function(speed) {
-		        this.multiway(speed, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180})
-		        return this;
-		    }
-
-		});
-
-		Crafty.e("2D, DOM, Color, LeftControls, Collision")
-			.color('rgb(0,0,255)')
-			.attr({ x: 300, y: 150, w: 10, h: 10,
-					dX: Crafty.math.randomInt(2, 5),
-					dY: Crafty.math.randomInt(2, 5) })
-			.leftControls(2)
+		var speed = 2
+		Crafty.e("Player, 2D, Canvas, Color, Multiway, Collision")
+			.color('rgb(0,255,0)')
+			.attr({ x: 190, y: 150, w: 20, h: 20 })
+			.multiway(speed, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180})
 			.bind('EnterFrame', function () {
-				//hit floor or roof
-				if (this.y <= 0) this.y++;
-				if (this.y >= 326) this.y--;
-				if (this.x <= 0) this.x++;
-				if (this.x >= 390) this.x--;
-
-				// this.x += this.dX;
-				// this.y += this.dY;
+				//hit sides
+				if (this.y <= 0){ this.y += speed; this.color('rgb(255,0,0)'); }
+				if (this.y >= 326){ this.y -= speed; this.color('rgb(255,0,0)'); }
+				if (this.x <= 0){ this.x += speed; this.color('rgb(255,0,0)'); }
+				if (this.x >= 390){ this.x -= speed; this.color('rgb(255,0,0)'); }
+			})
+			.onHit("Enemy",function(){
+				this.color('rgb(255,0,0)')
 			})
 
+		Crafty.e("Enemy, 2D, Canvas, Color, Collision")
+			.color('rgb(0,0,255)')
+			.attr({ x: 195, y: 20, w: 10, h: 10 })
+			.bind('EnterFrame', function(){
+				if(this.y < 300) this.y++;
+			})
+			.onHit("Sun", function(){
+				this.color('rgb(255,0,0)')
+			});
+
+		Crafty.e("Sun, 2D, Canvas, Color, Collision")
+			.color('r(255,255,0)')
+			.attr({ x: 125, y: 300, w: 150, h: 40 })
 
 	});
 
